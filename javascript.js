5b4b4a1ce5c1b25d0e680bea0b455a48a45de9da
MaqueiGH <105938140+MaqueiGH@users.noreply.github.com>
2022-06-27 13:04:21 +0200

    alto

---
+++ javascript.js
@@ -0,0 +1,7 @@
+let links = document.querySelector(".links")
+let navBtn = document.querySelector(".nav-button")
+
+navBtn.addEventListener("click", function() {
+    links.classList.toggle("show-links")
+})
+
